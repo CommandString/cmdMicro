faa11b84496c0b1332be7c3fb253b53bc811cf84
CommandString <bmxmikey2@gmail.com>
2022-12-23 08:38:39 -0500

    init

---
+++ index.php
@@ -0,0 +1,71 @@
+<?php
+
+use CommandString\CookieEncryption\Encryption;
+use CommandString\Cookies\Cookie;
+use CommandString\Pdo\Driver;
+use React\Socket\SocketServer;
+use Router\Http\Router;
+use CommandString\Env\Env;
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+
+require_once __DIR__ . "/vendor/autoload.php";
+
+#  _______ __   _ _   _  _____  ______  _____  __   _ _______ _______ __   _ _______ #
+# |______ | \  |  \  /    |   |_____/ |     | | \  | |  |  | |______ | \  |    |     #
+# |______ |  \_|   \/   __|__ |    \_ |_____| |  \_| |  |  | |______ |  \_|    |     #
+
+if (!file_exists(__DIR__."/env.json")) {
+    file_put_contents(__DIR__."/env.json", file_get_contents(__DIR__."/env.example.json"));
+    echo "Environment configuration does not exist, creating env.json!";
+}
+
+$env = Env::createFromJsonFile(__DIR__."/env.json");
+
+#  ______  _____  _     _ _______ _______  ______  #
+# |_____/ |     | |     |    |    |______ |_____/  #
+# |    \_ |_____| |_____|    |    |______ |    \_  #
+
+$router = new Router(new SocketServer("{$env->server->ip}:{$env->server->port}"), $env->server->dev);
+
+#  ______  _______ _______ _______ ______  _______ _______ _______ #
+# |     \ |_____|    |    |_____| |_____] |_____| |______ |______ #
+# |_____/ |     |    |    |     | |_____] |     | ______| |______ #
+
+if ($env->database->enabled) {
+    $env->driver = (new Driver)
+        ->withUsername($env->database->username)
+        ->withPassword($env->database->password)
+        ->withHost($env->database->host)
+        ->withPort($env->database->port)
+        ->connect()
+    ;
+}
+
+#  _______  _____   _____  _     _ _____ _______ _______ #
+# |       |     | |     | |____/    |   |______ |______ #
+# |_____  |_____| |_____| |    \_ __|__ |______ ______| #
+
+if ($env->cookies->enabled) {
+    $env->cookie = new Cookie(new Encryption($env->cookies->encryption_passphrase, $env->cookies->encryption_algo));
+}
+
+# _______ _  _  _ _____  ______ #
+#    |    |  |  |   |   |  ____ #
+#    |    |__|__| __|__ |_____| #
+
+$env->twig = new Environment(new FilesystemLoader(realpath($env->twigConfig->views)));
+
+#  ______  _____  _     _ _______ _______ _______ #
+# |_____/ |     | |     |    |    |______ |______ #
+# |    \_ |_____| |_____|    |    |______ ______| #
+$router
+    ->get("/", [Routes\Home::class, "main"])
+    ->get("/.*(".\Routes\Files\Files::generateRegex().")", [\Routes\Files\Files::class, "main"])
+    ->map404("/(.*)", [\Routes\ErrorHandler::class, "handle404"])
+    ->map500("/(.*)", [\Routes\ErrorHandler::class, "handle500"])
+;
+
+$router->listen();
+
+echo "Listening on {$env->server->ip}:{$env->server->port}";
