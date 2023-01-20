f6e72631cb1fdbbcce24e65dd26d562156fdff7b
commandstring <bmxmikey2@gmail.com>
2023-01-20 03:01:26 -0500

    Added proper 500 handler

--- routes/ErrorHandler.php
+++ routes/ErrorHandler.php
@@ -4,21 +4,21 @@ namespace Routes;
 
 use CommandString\Env\Env;
 use HttpSoft\Response\HtmlResponse;
-use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
+use React\Http\Message\ServerRequest;
 
 class ErrorHandler {
-    public static function handle404(RequestInterface $req): ResponseInterface
+    public static function handle404(ServerRequest $req): ResponseInterface
     {
         return new HtmlResponse(Env::get()->twig->render("errors/404.html", [
             "uri" => $req->getRequestTarget()
         ]), 404);
     }
 
-    public static function handle500(): ResponseInterface
+    public static function handle500(ServerRequest $req): ResponseInterface
     {
-        return new HtmlResponse(Env::get()->twig->render("errors/404.html", [
-            "uri" => $_SERVER["REQUEST_URI"]
+        return new HtmlResponse(Env::get()->twig->render("errors/500.html", [
+            "uri" => $req->getRequestTarget()
         ]), 500);
     }
 }
